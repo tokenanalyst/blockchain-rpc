d1041488a22a05c24d34d808067322258947224b
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2019-10-28 17:28:48 +0000

    moved file contents; added LICENSE badge

--- src/main/scala/BitcoinRPC.scala
+++ src/main/scala/BitcoinRPC.scala
@@ -1,3 +1,19 @@
+/**
+  * Licensed to the Apache Software Foundation (ASF) under one or more
+  * contributor license agreements.  See the NOTICE file distributed with
+  * this work for additional information regarding copyright ownership.
+  * The ASF licenses this file to You under the Apache License, Version 2.0
+  * (the "License"); you may not use this file except in compliance with
+  * the License.  You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package io.tokenanalyst.bitcoinrpc
 
 import org.http4s.circe.CirceEntityDecoder._
@@ -6,14 +22,18 @@ import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.io._
 import org.http4s.headers.{Authorization, _}
+import org.http4s.Uri
 import org.http4s.{BasicCredentials, MediaType, Request}
+
 import io.circe.{Decoder, Encoder, Json}
 import io.circe.syntax._
 import io.circe.generic.auto._
+
 import cats.effect.{IO, ContextShift, Resource}
 import cats.effect.ContextShift
+
 import Protocol._
-import org.http4s.Uri
+
 
 import scala.concurrent.ExecutionContext
 import org.http4s.client.blaze.BlazeClientBuilder
