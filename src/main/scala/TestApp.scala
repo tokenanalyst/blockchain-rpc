05806c36f5f1b92ec49d32196b74ac338322885f
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2019-10-28 10:30:53 +0000

    changed test app to include ENV vars

--- src/main/scala/TestApp.scala
+++ src/main/scala/TestApp.scala
@@ -1,13 +1,26 @@
 package io.tokenanalyst.jsonrpc
 
+import scala.util.{Success, Failure}
 import cats.effect.{IO, IOApp, ExitCode}
 
 object TestApp extends IOApp {
-  val config = Config("http://172.31.32.41:8332", "tokenanalyst","")
 
-  def run(args: List[String]): IO[ExitCode] = for { 
-    block <- Simple.getBlock(config, "000000000000000000100ed429cdd0f596a35317d0c7f754cf6f4e5110ce6307")
-    tx <- Simple.getTransaction(config, block.tx.head)
-    _ <- IO { println(tx) }
-  } yield ExitCode(0)
+  def getConfig = {
+    (sys.env.get("PASSWORD"), sys.env.get("USER"), sys.env.get("RPC_URL")) match {
+      case (Some(pass), Some(user), Some(url)) =>
+        Success(Config(url, user, pass))
+      case _ => Failure(new Exception("Pass RPC_URL, USER, PASSWORD."))
+    }
+  }
+
+  def run(args: List[String]): IO[ExitCode] = 
+    for {
+      config <- IO.fromTry(getConfig)
+      block <- Simple.getBlock(
+        config,
+        "000000000000000000100ed429cdd0f596a35317d0c7f754cf6f4e5110ce6307"
+      )
+      tx <- Simple.getTransaction(config, block.tx.head)
+      _ <- IO { println(tx) }
+    } yield ExitCode(0)
 }
